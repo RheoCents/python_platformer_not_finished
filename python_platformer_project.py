27f99da09d523af7408c08f0bf1e720eab2daa9a
unknown <centinaraphael0157@gmail.com>
2024-02-02 22:52:11 +0800

    Game setup done

--- python_platformer_project.py
+++ python_platformer_project.py
@@ -10,9 +10,27 @@ pygame.init()
 pygame.display.set_caption("Rheo's Platformer")
 
 Bg_color = (255,255,255)
-Width, Height =  100, 700
+Width, Height =  1000, 700
 FPS = 60
 Player_velocity = 5
 
 #display ---------------------------------------------------
 game_screen = pygame.display.set_mode((Width, Height))
+
+
+#main function -------------------------------------------
+def main(window):
+    clock =  pygame.time.Clock()
+    game_on = True
+    while game_on:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_on = False
+                break
+    
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(game_screen)
